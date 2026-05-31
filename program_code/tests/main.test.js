8fe09fb98322e380c0c75dd3c693e8be7382e8d2
Kutsevych Anna <anna.kutsevych@nure.ua>
2026-05-31 18:17:05 +0300

    corrected error

--- program_code/tests/main.test.js
+++ program_code/tests/main.test.js
@@ -128,7 +128,7 @@ test("extra click on generateBtn shows generated result and saves history", () =
   `;
 
   jest.resetModules();
-  const app = require("../main");
+  require("../main");
 
   document.getElementById("message").value = "я вивчив фігму";
   document.getElementById("tone").value = "formal";  document.getElementById("generateBtn").click();  expect(document.getElementById("result").textContent)
